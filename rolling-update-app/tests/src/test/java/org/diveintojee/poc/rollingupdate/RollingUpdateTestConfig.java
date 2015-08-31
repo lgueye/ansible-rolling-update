d0ea0fd56b55814c27fc159ca2e4425640f71b6c
louis gueye <louis.gueye@gmail.com>
2015-08-31 10:30:16 +0200

    pom clean up

--- rolling-update-app/tests/src/test/java/org/diveintojee/poc/rollingupdate/RollingUpdateTestConfig.java
+++ rolling-update-app/tests/src/test/java/org/diveintojee/poc/rollingupdate/RollingUpdateTestConfig.java
@@ -1,17 +1,10 @@
 package org.diveintojee.poc.rollingupdate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
-
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
-import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 
 /**
  * @author louis.gueye@gmail.com
@@ -26,25 +19,25 @@ public class RollingUpdateTestConfig {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
-    @Bean
-    public RestOperations buildRestTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(0, mappingJacksonHttpMessageConverter());
-        return restTemplate;
-    }
+//    @Bean
+//    public RestOperations buildRestTemplate() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.getMessageConverters().add(0, mappingJacksonHttpMessageConverter());
+//        return restTemplate;
+//    }
 
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setObjectMapper(jacksonSerializer());
-        return converter;
-    }
-
-    @Bean
-    public ObjectMapper jacksonSerializer() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(INDENT_OUTPUT, true);
-        objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return objectMapper;
-    }
+//    @Bean
+//    public MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter() {
+//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//        converter.setObjectMapper(jacksonSerializer());
+//        return converter;
+//    }
+//
+//    @Bean
+//    public ObjectMapper jacksonSerializer() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.configure(INDENT_OUTPUT, true);
+//        objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        return objectMapper;
+//    }
 }
